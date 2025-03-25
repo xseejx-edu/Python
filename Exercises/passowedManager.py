84af2553b3efd78efd5ed833a7f57154c80a73fe
xseejx-edu <simone.comin@iiseinaudiscarpa.edu.it>
2025-03-25 16:35:33 +0100

    Updated passwordmanager

--- Exercises/passowedManager.py
+++ Exercises/passowedManager.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 def newPasswordfile(password_number=1, pass_length=10):
     word_list = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()" 
@@ -12,29 +13,44 @@ def newPasswordfile(password_number=1, pass_length=10):
     manageFile(filePath=fileName, mode="w", list=pass_list)
 
 def manageFile(filePath=None, mode="r", list=None):
-    with open(file=filePath, mode=mode) as file:
-        if mode == "w":
-            for _ in list:
-                file.write(_+"\n")
-        elif mode == "r":
-            print(file.read())
+    try:
+        with open(file=filePath, mode=mode) as file:
+            if mode == "w":
+                for _ in list:
+                    file.write(_[::-1]+"\n")
+            elif mode == "r":
+                print(file.read())
+    except Exception as e:
+        print(e)
 
 if __name__ == "__main__":
     mode = 0
     while mode != 4:
-        mode = int(input("""
-                    [1] Generate password file
-                    [2] Read password file
-                    [3] Clear password file 
-                    [4] Exit\n
-                    [i] >> """))
+        try:
+            mode = int(input("""
+                        [1] Generate password file
+                        [2] Read password file
+                        [3] Clear password file 
+                        [4] Exit\n
+                        [i] >> """))
+        except Exception as e:
+            print(e)
+            continue
         if mode==1:
             password_number = 0
             pass_length = 0
             while password_number<1 or password_number>50:
-                password_number = int(input("[?] Insert number of passwords [1-50] >>  "))
-            while pass_length<10 or password_number>20:
-                pass_length = int(input("[?] Insert length of passwords [10-20] >>  "))
+                try:
+                    password_number = int(input("[?] Insert number of passwords [1-50] >>  "))
+                except Exception as e:
+                    print(e)
+                    
+            while pass_length<10 or pass_length>20:
+                try:
+                    pass_length = int(input("[?] Insert length of passwords [10-20] >>  "))
+                except Exception as e:
+                    print(e)
+                  
             newPasswordfile(password_number=password_number, pass_length=pass_length)
         elif mode==2:
             fileName = input("[?] Insert name of file > ")
@@ -42,7 +58,3 @@ if __name__ == "__main__":
         elif mode==3:
             fileName = input("[?] Insert name of file > ")
             manageFile(filePath=fileName, mode="w", list=[" " for _ in range(50)])
-
-        
-    
-    
